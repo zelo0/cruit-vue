90ab7ac14330615cdfbca59814b63472c0bb85d4
zelo0 <minorSunbi@gmail.com>
2022-02-11 00:52:20 +0900

    회원가입 폼  vuelidate로 검증

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,10 +1,18 @@
 <template lang="">
-  <div class="bg-red-50 flex p-2">
+  <div
+    class="bg-red-50 flex justify-between py-2 px-5 border-b-2 border-gray-300 shadow"
+  >
     <div class="basis-1/4 mr-2">로고</div>
+
     <div
-      class="ring-1 ring-red-500 ring-offset-1 ring-offset-red-100 flex-initial"
+      class="ring-1 ring-red-500 ring-offset-1 ring-offset-red-100 flex-initial w-1/2"
     >
       검색
+    </div>
+
+    <div class="flex gap-5">
+      <nuxt-link to="/login" class="font-bold">로그인</nuxt-link>
+      <nuxt-link to="/join" class="font-bold">회원가입</nuxt-link>
     </div>
   </div>
 </template>
---
+++ pages/join.vue
@@ -0,0 +1,13 @@
+<template>
+  <div
+    class="flex items-center justify-center h-full bg-gradient-to-tl from-gray-700 to-blue-50"
+  >
+    <JoinForm />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'JoinPage',
+}
+</script>
