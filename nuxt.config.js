d5a1816fe5b7e64f21b868950d6ddbdae54af31e
zelo0 <minorSunbi@gmail.com>
2022-02-18 14:55:23 +0900

    vuex-persist 추가로 새로 고침 시도에 헤더에 닉네임 표시

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,10 @@ export default {
   css: ['~/assets/css/main.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [{ src: '~plugins/vuelidate.js', mode: 'client' }],
+  plugins: [
+    { src: '~plugins/vuelidate.js', mode: 'client' },
+    { src: '~plugins/vuex-persist', mode: 'client' },
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
