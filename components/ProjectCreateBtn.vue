2f535db7a863a4e62231f8b7f0b56303e6876467
zelo0 <minorSunbi@gmail.com>
2022-02-28 00:55:50 +0900

    프로젝트 생성 버튼, view 추가

---
+++ components/ProjectCreateBtn.vue
@@ -0,0 +1,15 @@
+<template lang="">
+  <div class="my-2 text-right">
+    <button @click="onclick" class="myBtn inline-block">프로젝트 생성</button>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    onclick() {
+      this.$router.push('/project-create')
+    },
+  },
+}
+</script>
+<style lang=""></style>
---
+++ components/ProjectCreateForm.vue
@@ -0,0 +1,26 @@
+<template lang="">
+  <div>
+    <div class="font-extrabold text-xl">프로젝트 공고</div>
+    <form class="gap-5 grid grid-flow-row mt-5">
+      <div>
+        <label class="boldAndSpace" for="title">제목</label>
+        <input class="w-full mt-2" type="text" id="title" />
+      </div>
+      <hr />
+
+      <div>
+        <label class="boldAndSpace" for="title">내용</label>
+        <client-only placeholder="Loading....">
+          <MarkEditor />
+        </client-only>
+      </div>
+      <hr />
+
+      <label for="desc"></label>
+    </form>
+  </div>
+</template>
+<script>
+export default {}
+</script>
+<style lang=""></style>
