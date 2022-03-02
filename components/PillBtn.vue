cccb5b7a12b2d878173ee69f26a9bd52e7ed5f4e
zelo0 <minorSunbi@gmail.com>
2022-03-02 14:23:01 +0900

    이미지와 텍스트 보여주는 클릭 가능한 알약형 버튼

---
+++ components/PillBtn.vue
@@ -0,0 +1,14 @@
+<template lang="">
+  <div class="flex-wrap ring-2 rounded-md inline-flex mr-5 mt-2 box-content">
+    <img :src="img" class="w-6 object-cover rounded-l-md"></img>
+    <div class="leading-4 py-1 px-3 font-semibold">{{ name }}</div>
+  </div>
+</template>
+<script>
+export default {
+  props: ['name', 'img' ],
+}
+</script>
+<style lang="">
+  
+</style>
