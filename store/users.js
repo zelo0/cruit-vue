4f9c48e35256f46afdd4274eaea35234ad7d19d3
zelo0 <minorSunbi@gmail.com>
2022-03-04 15:13:47 +0900

    유저 검색하기 위한 vuex 파일 생성

---
+++ store/users.js
@@ -0,0 +1,62 @@
+export const state = () => ({
+  stackFilters: [],
+  users: [],
+  hasError: false,
+  hasPrevious: false,
+  hasNext: false,
+  totalPage: 0,
+  currentPage: 0,
+})
+
+export const mutations = {
+  addStackFilter(state, text) {
+    state.stackFilters.push(text)
+  },
+  removeStackFilter(state, text) {
+    state.stackFilters.splice(state.stackFilters.indexOf(text), 1)
+  },
+  setUsers(state, users) {
+    state.users = users
+  },
+  setHasError(state, hasError) {
+    state.hasError = hasError
+  },
+  setHasPrevious(state, hasPrevious) {
+    state.hasPrevious = hasPrevious
+  },
+  setHasNext(state, hasNext) {
+    state.hasNext = hasNext
+  },
+  setTotalPage(state, totalPage) {
+    state.totalPage = totalPage
+  },
+  setCurrentPage(state, currentPage) {
+    state.currentPage = currentPage
+  },
+  increaseCurrentPage(state) {
+    state.currentPage += 1
+  },
+  decreaseCurrentPage(state) {
+    state.currentPage -= 1
+  },
+}
+
+export const actions = {
+  async getUsersByFilter({ commit, state }) {
+    await this.$axios
+      .$get(
+        `/users?q=${state.stackFilters.join(';')}&page=${state.currentPage}`
+      )
+      .then((res) => {
+        commit('setUsers', res.data)
+        commit('setHasPrevious', res.hasPrevious)
+        commit('setHasNext', res.hasNext)
+        commit('setTotalPage', res.totalPage)
+        commit('setCurrentPage', res.currentPage)
+        commit('setHasError', false)
+      })
+      .catch((err) => {
+        commit('setHasError', true)
+      })
+  },
+}
