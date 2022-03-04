81c88e3da8604030ea9d379ff372307d8a92d07a
zelo0 <minorSunbi@gmail.com>
2022-03-04 15:11:04 +0900

    유저 검색 시 보여지는 각 유저의 컴포넌트 (명함 같은 역할)

---
+++ components/UserCard.vue
@@ -0,0 +1,45 @@
+<template lang="">
+  <div
+    class="relative ring-2 p-2 rounded-md"
+    :class="{
+      'ring-green-400': user.position == 'BACKEND',
+      'ring-purple-400': user.position == 'FRONTEND',
+      'ring-yellow-400': user.position == 'DESIGN',
+    }"
+  >
+    <div class="flex">
+      <Avatar :url="user.profile" />
+      <p class="font-bold leading-10 ml-1">{{ user.name }}</p>
+    </div>
+
+    <div>
+      <h3>
+        {{ user.position }}
+      </h3>
+    </div>
+
+    <div>
+      <OverlappingAvatars :imageList="user.stacks" />
+    </div>
+
+    <!-- 오버레이 (대표 url) -->
+    <div
+      class="absolute right-0 top-0 bg-gray-700 rounded-tr-md text-white p-1"
+    >
+      <a :href="user.github" target="_blank">LINK</a>
+    </div>
+
+    <!-- 리더 가능 여부 -->
+    <div
+      class="absolute left-0 top-0 opacity-30 rounded-tl-md bg-red-400 text-white p-1"
+    >
+      <p>LEADER</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ['user'],
+}
+</script>
+<style lang=""></style>
