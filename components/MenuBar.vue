d25aa3e90319504cefd46bb9fa6f8d2b17458620
zelo0 <minorSunbi@gmail.com>
2022-03-04 15:02:57 +0900

    프로젝트 또는 유저 검색할 수 있는 탭이 있는 메뉴바 생성

---
+++ components/MenuBar.vue
@@ -0,0 +1,10 @@
+<template lang="">
+  <div class="flex gap-5 border-b-4 border-blue-500 mb-5">
+    <NuxtLink to="/"><h2>프로젝트</h2></NuxtLink>
+    <NuxtLink to="/users"><h2>유저</h2></NuxtLink>
+  </div>
+</template>
+<script>
+export default {}
+</script>
+<style lang=""></style>
