cc85b4d995a314e94930ca4b1330199225f41b09
zelo0 <minorSunbi@gmail.com>
2022-03-04 15:02:29 +0900

    초기 텍스트 말고 placeholder로 변경

--- components/MarkEditor.vue
+++ components/MarkEditor.vue
@@ -2,7 +2,6 @@
   <div class="mt-2 ring-2">
     <ToastEditor
       ref="toasteditor"
-      :initialValue="editorText"
       :options="editorOptions"
       height="500px"
       initialEditType="wysiwyg"
@@ -14,8 +13,8 @@
 export default {
   data() {
     return {
-      editorText: '이 곳에 프로젝트원 모집 글을 작성해주세요',
       editorOptions: {
+        placeholder: '이 곳에 프로젝트원 모집 글을 작성해주세요',
         hideModeSwitch: true,
         // theme: 'dark',
       },
