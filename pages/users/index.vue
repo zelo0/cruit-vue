ecb58e45883cb74b7fa334570930c7aca7d9104f
zelo0 <minorSunbi@gmail.com>
2022-03-04 15:13:16 +0900

    유저 검색 페이지 생성

---
+++ pages/users/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <TheHeader />
+    <div class="container md:w-4/5 p-4">
+      <MenuBar />
+      <!-- <ProjectSearchFilter /> -->
+      <UserList />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserPage',
+}
+</script>
