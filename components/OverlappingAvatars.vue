56dd3ea6520d78a8f16c5ad8579be2b88d875a25
zelo0 <minorSunbi@gmail.com>
2022-03-04 15:03:26 +0900

    태그 하나로 변경

--- components/OverlappingAvatars.vue
+++ components/OverlappingAvatars.vue
@@ -1,10 +1,6 @@
 <template lang="">
   <div class="flex -space-x-2 overflow-hidden">
-    <Avatar
-      v-for="(image, index) in imageList"
-      :key="index"
-      :url="image"
-    ></Avatar>
+    <Avatar v-for="(image, index) in imageList" :key="index" :url="image" />
   </div>
 </template>
 <script>
