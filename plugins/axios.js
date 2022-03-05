b9ae58b71fc80449d8ea51762a4253a79ff545e3
zelo0 <minorSunbi@gmail.com>
2022-03-06 00:33:23 +0900

    403 오류 시 403 오류 페이지로 이동

--- plugins/axios.js
+++ plugins/axios.js
@@ -2,6 +2,8 @@ export default function ({ $axios, redirect, store, route }) {
   $axios.onError((error) => {
     if (error.response.status === 401) {
       redirect('/login')
+    } else if (error.response.status == 403) {
+      redirect('/error/403')
     }
   })
 }
