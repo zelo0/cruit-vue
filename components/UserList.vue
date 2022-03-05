72f7b89a15b33a4e2fdf63de94b9d76f6e80f2e6
zelo0 <minorSunbi@gmail.com>
2022-03-06 00:31:23 +0900

    검색 결과 있는지에 따라 다른 렌더링

--- components/UserList.vue
+++ components/UserList.vue
@@ -1,8 +1,21 @@
 <template lang="">
   <div>
-    <div v-if="!hasError" class="grid xl:grid-cols-4 sm:grid-cols-2 gap-10">
-      <UserCard v-for="(item, index) in users" :key="index" :user="item" />
+    <div v-if="!hasError">
+      <!-- 검색 결과 있으면 -->
+      <div
+        v-if="users.length"
+        class="grid xl:grid-cols-4 sm:grid-cols-2 gap-10"
+      >
+        <UserCard v-for="(item, index) in users" :key="index" :user="item" />
+      </div>
+
+      <!-- 검색 결과 없으면 -->
+      <div v-else class="text-center">
+        <h2>검색결과가 없습니다</h2>
+      </div>
     </div>
+
+    <!-- 에러 발생 시 -->
     <div v-else class="text-red-400 font-bold text-2xl text-center">
       에러가 발생했습니다
     </div>
