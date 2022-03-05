3bd64a8bcd74b20daeb347993d768d88eee50fef
zelo0 <minorSunbi@gmail.com>
2022-03-06 00:33:01 +0900

    오류 페이지(403) 추가

---
+++ pages/error/403.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <TheHeader />
+    <div class="container md:w-4/5 p-4">
+      <MenuBar />
+      <ProjectCreateBtn />
+      <ProjectSearchFilter />
+      <ProjectList />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '403-page',
+}
+</script>
