726f3ebc6a05135e02c7896deaf58cdda40ad9fe
zelo0 <minorSunbi@gmail.com>
2022-03-07 16:25:47 +0900

    PartProposalItem.vue 생성

---
+++ components/PartProposalItem.vue
@@ -0,0 +1,29 @@
+<template lang="">
+  <div class="px-5 py-1 border-2 ver-gap1-grid" @click="onclick">
+    <div>
+      <h3>프로젝트명</h3>
+      <p>{{ part.projectName }}</p>
+    </div>
+
+    <div>
+      <h3>파트</h3>
+      <p>{{ part.position }}</p>
+    </div>
+
+    <div>
+      <h3>모집 공개 상태</h3>
+      <p>{{ part.status }}</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ['part'],
+  methods: {
+    onclick() {
+      this.$emit('clicked', this.part)
+    },
+  },
+}
+</script>
+<style lang=""></style>
