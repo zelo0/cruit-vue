cef3dd89dc2924d61dac3eb9e9fb0fc2d4b09213
zelo0 <minorSunbi@gmail.com>
2022-03-08 14:51:52 +0900

    늘어난 텍스트에 맞게 높이를 변경해주는 textarea 컴포넌트

---
+++ components/AutoResizingTextArea.vue
@@ -0,0 +1,32 @@
+<template lang="">
+  <div>
+    <textarea v-model="content" @keyup="resize" ref="textarea" />
+  </div>
+</template>
+<script>
+export default {
+  props: ['initialContent'],
+  data() {
+    return {
+      content: '',
+    }
+  },
+  methods: {
+    resize() {
+      this.$refs.textarea.style.height = this.$refs.textarea.scrollHeight + 'px'
+    },
+    getContent() {
+      return this.content
+    },
+    clearContent() {
+      this.content = ''
+    },
+  },
+  created() {
+    if (this.initialContent) {
+      this.content = this.initialContent
+    }
+  },
+}
+</script>
+<style lang=""></style>
