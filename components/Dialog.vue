f210f7c33489ce45bfe3e1dfd13216ccbeebf2a6
zelo0 <minorSunbi@gmail.com>
2022-03-08 14:52:24 +0900

    Dialog를 컴포넌트로 분리

---
+++ components/Dialog.vue
@@ -0,0 +1,65 @@
+<template lang="">
+  <div
+    v-show="isDialogShown"
+    class="bg-black bg-opacity-50 flex z-10 fixed left-0 top-0 w-screen h-screen items-center justify-center"
+    @click="isDialogShown = false"
+  >
+    <!-- 팝업 창 -->
+    <div class="bg-white py-10 px-7 rounded-lg relative" @click.stop>
+      <div class="text-right">
+        <button
+          class="font-bold text-xl absolute right-4 top-1.5"
+          @click="onCancelProposal"
+        >
+          X
+        </button>
+      </div>
+
+      <div>
+        <!-- 메시지 -->
+        <div>
+          <slot name="message"></slot>
+        </div>
+        <!-- 버튼들 -->
+        <div class="flex justify-evenly mt-5 gap-5">
+          <button
+            class="bg-gray-200 hover:bg-gray-300 w-full h-10 rounded-md"
+            @click="onAnswerYes"
+          >
+            예
+          </button>
+          <button
+            class="bg-gray-200 hover:bg-gray-300 w-full h-10 rounded-md"
+            @click="onAnswerNo"
+          >
+            아니오
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      isDialogShown: false,
+    }
+  },
+  methods: {
+    onCancelProposal() {
+      this.isDialogShown = false
+    },
+    onAnswerYes() {
+      this.$emit('answeredYes')
+    },
+    onAnswerNo() {
+      this.$emit('answeredNo')
+    },
+    show() {
+      this.isDialogShown = true
+    },
+  },
+}
+</script>
+<style lang=""></style>
