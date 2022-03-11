955477aad4e9b46ff182c6f9ae10b085d7cd963c
zelo0 <minorSunbi@gmail.com>
2022-03-11 09:51:09 +0900

    페이징에서 다른 페이지 선택 시 스크롤을 위로 올려주기

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -3,13 +3,26 @@
     <div class="container md:w-4/5 p-4">
       <MenuBar />
       <UserSearchFilter />
+      <span ref="anchor"></span>
       <UserList />
     </div>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 export default {
-  name: 'UserPage',
+  computed: {
+    ...mapState({
+      currentPage: (state) => state.users.currentPage,
+    }),
+  },
+  watch: {
+    // 검색 페이지 번호가 변하면 스크롤 위로 이동시키기
+    currentPage() {
+      const anchor = this.$refs.anchor
+      anchor.scrollIntoView({ behavior: 'smooth' })
+    },
+  },
 }
 </script>
