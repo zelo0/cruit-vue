aa4ac245ade8be4e3ed375f1590b5f2ba49a8c3d
zelo0 <minorSunbi@gmail.com>
2022-03-18 00:17:58 +0900

    화살표 아이콘 추ㅏㄱ

--- plugins/vue-unicons.js
+++ plugins/vue-unicons.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import Unicon from 'vue-unicons/dist/vue-unicons-vue2.umd'
-import { uniBell, uniAngleRightB, uniTimes } from 'vue-unicons/dist/icons'
+import {
+  uniBell,
+  uniAngleRightB,
+  uniTimes,
+  uniArrowRight,
+} from 'vue-unicons/dist/icons'
 
-Unicon.add([uniBell, uniAngleRightB, uniTimes])
+Unicon.add([uniBell, uniAngleRightB, uniTimes, uniArrowRight])
 Vue.use(Unicon)
