95f0224396394e3a0fa32f803fa9ee79762b8747
zelo0 <minorSunbi@gmail.com>
2022-03-18 00:13:02 +0900

    파트 수정 페이지 생성

---
+++ pages/parts/modify/_id.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div class="ver-gap5-grid">
+      <PartModifyForm />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // middleware: 'authenticated',
+}
+</script>
