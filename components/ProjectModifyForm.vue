12bbd77f8c9ef85dfe5f812f6461f3e713d52429
zelo0 <minorSunbi@gmail.com>
2022-03-18 00:07:51 +0900

    프로젝트 상태 변경하는 부분 추가

--- components/ProjectModifyForm.vue
+++ components/ProjectModifyForm.vue
@@ -1,7 +1,9 @@
 <template lang="">
   <div>
-    <form class="ver-gap5-grid" @submit.prevent="onsubmit">
-      <div class="font-extrabold text-xl">프로젝트 공고</div>
+    <form class="ver-gap5-grid" @submit.prevent>
+      <h1>프로젝트 공고</h1>
+
+      <!-- 이름 변경 -->
       <div>
         <label class="boldAndSpace" for="title">제목</label>
         <input
@@ -13,6 +15,7 @@
       </div>
       <hr />
 
+      <!-- 설명 변경 -->
       <div>
         <label class="boldAndSpace" for="title">내용</label>
         <client-only placeholder="Loading....">
@@ -25,7 +28,42 @@
       </div>
 
       <div class="my-2 text-right">
-        <button class="myBtn inline-block">변경</button>
+        <button class="myBtn inline-block" @click="changeContent">변경</button>
+      </div>
+
+      <hr />
+
+      <!-- 상태 변경 -->
+      <div class="ver-gap2-grid">
+        <div class="boldAndSpace">상태</div>
+        <label for="togleButton" class="cursor-pointer">
+          <div class="flex gap-3 items-center">
+            <span class="cursor-pointer"> 비공개 </span>
+
+            <!-- toggle -->
+            <div class="relative">
+              <input
+                id="togleButton"
+                type="checkbox"
+                class="hidden"
+                @change="changeStatus"
+                v-model="project.status"
+                true-value="PUBLIC"
+                false-value="PRIVATE"
+              />
+              <!-- path -->
+              <div
+                class="toggle-path bg-gray-200 w-9 h-5 rounded-full shadow-inner"
+              ></div>
+              <!-- crcle -->
+              <div
+                class="toggle-circle absolute w-3.5 h-3.5 bg-white rounded-full shadow inset-y-0 left-0 transition-all duration-300 ease-in-out"
+              ></div>
+            </div>
+
+            <span class="cursor-pointer"> 공개 </span>
+          </div>
+        </label>
       </div>
     </form>
   </div>
@@ -37,11 +75,12 @@ export default {
       project: {
         name: '',
         description: '',
+        status: 'PUBLIC',
       },
     }
   },
   methods: {
-    async onsubmit() {
+    async changeContent() {
       const description = this.$refs.editor.getHtml()
       await this.$axios
         .$patch('/projects/text', {
@@ -50,11 +89,23 @@ export default {
           description,
         })
         .then((res) => {
-          const savedProjectId = res.data.projectId
-          this.$router.replace(`/projects/${savedProjectId}`)
+          alert('변경됐습니다')
         })
         .catch((err) => {
           console.log('에러 발생')
+        })
+    },
+    async changeStatus() {
+      await this.$axios
+        .$patch('/projects/status', {
+          id: this.$route.params.id,
+          status: this.project.status,
+        })
+        .then((res) => {
+          alert('변경됐습니다')
+        })
+        .catch((err) => {
+          console.log('에러')
         })
     },
   },
@@ -70,4 +121,4 @@ export default {
   },
 }
 </script>
-<style lang=""></style>
+<style lang="css" scoped></style>
