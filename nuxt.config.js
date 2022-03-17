0ae1a13d05229cbd67f3549532b4e026b57337ad
zelo0 <minorSunbi@gmail.com>
2022-03-18 00:05:23 +0900

    nuxt 메타데이터 변경

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,14 +1,18 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'cruit-tailwind',
+    title: 'cruit',
     htmlAttrs: {
-      lang: 'en',
+      lang: 'ko',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: '프로젝트 멤버를 찾으세요',
+      },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
