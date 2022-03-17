23db79b1b132cbcc8839fcc4c92b41ba75757cda
zelo0 <minorSunbi@gmail.com>
2022-03-18 00:13:29 +0900

    프로젝트 수정 페이지 생성

---
+++ pages/projects/modify/_id.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="ver-gap5-grid">
+    <ProjectModifyForm />
+    <hr />
+
+    <NuxtLink to="/users" class="flex justify-between">
+      <h2>지금 바로 함께 할 멤버를 찾아보세요</h2>
+      <client-only>
+        <unicon name="angle-right-b" />
+      </client-only>
+    </NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProjectModifyPage',
+  middleware: 'authenticated',
+}
+</script>
