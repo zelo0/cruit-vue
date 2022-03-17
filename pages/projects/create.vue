12332bee77b103e392698850825ab08abd66bd58
zelo0 <minorSunbi@gmail.com>
2022-03-18 00:13:19 +0900

    프로젝트 생성 페이지 생성

---
+++ pages/projects/create.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div class="">
+      <ProjectCreateForm />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProjectCreatePage',
+  middleware: 'authenticated',
+}
+</script>
