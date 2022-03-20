54f9fba936551ced7934b4b729a9bd247b23ac9a
zelo0 <minorSunbi@gmail.com>
2022-03-20 20:45:28 +0900

    screen size에 xs 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,5 +7,13 @@ module.exports = {
     dark: {
       // backgroundcolor:
     },
+    screens: {
+      // xxs는 정상적으로 작동하지 않음
+      xs: '350px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
   },
 }
