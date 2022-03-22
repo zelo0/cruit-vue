3cd6e5261ef554e0b76b565e01b0ba54f980a403
zelo0 <minorSunbi@gmail.com>
2022-03-22 22:23:02 +0900

    axios의 url을 서버 url로 변경

--- nuxt.config.js
+++ nuxt.config.js
@@ -48,7 +48,8 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://localhost:8080/api/v1',
+    // baseURL: 'http://localhost:8080/api/v1', // local
+    baseURL: 'http://3.39.34.125:8080/api/v1', // ec2
     credentials: true,
   },
 
