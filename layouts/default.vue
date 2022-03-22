2493e58206c85b6432ceabc5ba9c891077bb107e
zelo0 <minorSunbi@gmail.com>
2022-03-22 22:02:09 +0900

    잘못된 페이지 번호를 로드하는 문제를 해결하기 위해서는 종료 시 vuex 제거가 필요함

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,8 +10,9 @@
 export default {
   mounted() {
     window.addEventListener('beforeunload', (event) => {
-      // window.localStorage.removeItem('vuex')
       // 이 코드로 인해 새로고침 시 vuex 날라감
+      // 이게 없으면 생기는 문제는 종료 후 다시 켰을 때 페이지 번호가 1이 아니라는거
+      window.localStorage.removeItem('vuex')
     })
   },
 }
