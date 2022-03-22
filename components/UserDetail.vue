8f65cc897c3cb43e9e6107bf6159529cbbc2a6b4
zelo0 <minorSunbi@gmail.com>
2022-03-22 22:00:46 +0900

    유저 이름 중앙에 오게 하기

--- components/UserDetail.vue
+++ components/UserDetail.vue
@@ -2,7 +2,7 @@
   <div>
     <div v-if="user" class="ver-gap5-grid">
       <!-- 프로필 이미지, 이름 -->
-      <div class="place-self-center">
+      <div class="place-self-center text-center">
         <img :src="user.profile" alt="유저 프로필 이미지" class="big-profile" />
         <h2 class="mt-3">{{ user.name }}</h2>
       </div>
