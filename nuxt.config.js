c01c633e41b563d995b2a6971ff0dba9152c6b80
zelo0 <minorSunbi@gmail.com>
2022-03-23 22:45:32 +0900

    https로 요청

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,7 +49,7 @@ export default {
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     // baseURL: 'http://localhost:8080/api/v1', // local
-    baseURL: 'http://www.cruitapp.com/api/v1', // ec2
+    baseURL: 'https://www.cruitapp.com/api/v1', // ec2
     credentials: true,
   },
 
