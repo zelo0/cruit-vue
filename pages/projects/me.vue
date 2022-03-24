fc04f68c8dd048ff2c1646762e2378ae03fc9913
zelo0 <minorSunbi@gmail.com>
2022-03-24 16:35:11 +0900

    내 프로젝트 페이지에서 삭제 시 화면에서 제거

--- pages/projects/me.vue
+++ pages/projects/me.vue
@@ -5,11 +5,12 @@
       프로젝트를 클릭하면 모든 프로젝트원이 볼 수 있는 공지방으로 이동합니다
     </div>
     <div class="grid xl:grid-cols-4 sm:grid-cols-2 gap-10">
-      <ProjectItem
+      <MyProjectItem
         v-for="(project, index) in projects"
         :key="project.id"
         :project="project"
         @clicked="moveToBoard"
+        @deleted="removeInPage"
       />
     </div>
   </div>
@@ -25,6 +26,12 @@ export default {
   methods: {
     moveToBoard(projectId) {
       this.$router.push(`/projects/${projectId}/boards`)
+    },
+    removeInPage(projectId) {
+      this.projects.splice(
+        this.projects.findIndex((project) => (project.id = projectId)),
+        1
+      )
     },
   },
   async fetch() {
