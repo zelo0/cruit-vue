f5f90182fc1be3fe2d9a5fc707832efe2dad0f0f
zelo0 <minorSunbi@gmail.com>
2022-03-24 19:29:59 +0900

    모바일에서의 자연스러운 움직임을 위해 hover 없이 active만 적용

--- components/SelectableStackItem.vue
+++ components/SelectableStackItem.vue
@@ -1,5 +1,5 @@
 <template lang="">
-  <div @click="onclick" :class="{'bg-blue-300': isSelected, 'bg-white': !isSelected}" class="cursor-pointer flex flex-wrap p-2 ring-2 rounded-md hover:bg-blue-100 duration-400">
+  <div @click="onclick" :class="{'bg-blue-300': isSelected, 'bg-white': !isSelected}" class="cursor-pointer flex flex-wrap p-2 ring-2 rounded-md  active:bg-blue-50 duration-400">
     <img :src="data.image" class="w-10 h-10 object-contain"></img>
     <div class="leading-10 ml-2 font-bold">{{ data.name }}</div>
   </div>
