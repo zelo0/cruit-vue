91d0433628a80242227748b6fa4996bf59f391a2
zelo0 <minorSunbi@gmail.com>
2022-03-27 23:39:12 +0900

    quill, purifier 관련 플러그인 파일 생성

---
+++ plugins/quill-editor.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import Quill from 'quill'
+import ImageResize from 'quill-image-resize-module'
+import VueQuillEditor from 'vue-quill-editor'
+
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+
+Vue.use(VueQuillEditor)
