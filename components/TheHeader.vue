2b7d9576e8cdb0c97cf462045c18ad837559b8ce
zelo0 <minorSunbi@gmail.com>
2022-03-28 00:31:43 +0900

    알림판 너비 조절해주기 위해 dom에서 위치 변경

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -39,47 +39,6 @@
           >
             {{ notificationCount }}
           </div>
-
-          <!-- 알림판 -->
-          <TransitionSlideUpDown>
-            <div
-              v-show="isShowingNotification"
-              class="absolute z-40 top-7 right-2 border-2 border-black bg-white dark:bg-night-100 dark:text-white rounded-md font-normal w-60 drop-shadow-xl h-80 overflow-y-auto"
-            >
-              <!-- 알림 영역 -->
-              <div>
-                <div
-                  v-for="(notification, index) in notifications"
-                  :key="index"
-                  class="border-b-2 p-2 cursor-pointer"
-                  @click="setToRead(notification)"
-                >
-                  <NuxtLink
-                    :to="{
-                      path:
-                        notification.type == 'question'
-                          ? `/projects/${notification.relatedId}`
-                          : '/proposals/me',
-                    }"
-                  >
-                    <span>
-                      {{ notification.message }}
-                    </span>
-                  </NuxtLink>
-                </div>
-              </div>
-              <!-- 전체 보기 버튼 -->
-              <div class="">
-                <NuxtLink to="/proposals/me">
-                  <div
-                    class="bg-gray-100 dark:text-white dark:bg-night-300 absolute bottom-0 w-full cursor-pointer p-1 text-center"
-                  >
-                    전체 보기
-                  </div>
-                </NuxtLink>
-              </div>
-            </div>
-          </TransitionSlideUpDown>
         </div>
 
         <!-- 프로젝트 바로가기 -->
@@ -95,6 +54,46 @@
         <span class="cursor-pointer" @click="logout">로그아웃</span>
       </div>
 
+      <!-- 알림판 -->
+      <TransitionSlideUpDown>
+        <div
+          v-show="isShowingNotification"
+          class="absolute z-40 top-9 right-7 border-2 border-black bg-white dark:bg-night-100 dark:text-white rounded-md font-normal w-1/2 drop-shadow-xl max-h-80 overflow-y-scroll"
+        >
+          <!-- 알림 영역 -->
+          <div class="">
+            <div
+              v-for="(notification, index) in notifications"
+              :key="index"
+              class="border-b-2 p-2 cursor-pointer"
+              @click="setToRead(notification)"
+            >
+              <NuxtLink
+                :to="{
+                  path:
+                    notification.type == 'question'
+                      ? `/projects/${notification.relatedId}`
+                      : '/proposals/me',
+                }"
+              >
+                <span>
+                  {{ notification.message }}
+                </span>
+              </NuxtLink>
+            </div>
+          </div>
+          <!-- 전체 보기 버튼 -->
+          <div class="">
+            <NuxtLink to="/proposals/me">
+              <div
+                class="sticky bg-gray-100 dark:text-white dark:bg-night-300 bottom-0 w-full cursor-pointer p-1 text-center"
+              >
+                전체 보기
+              </div>
+            </NuxtLink>
+          </div>
+        </div>
+      </TransitionSlideUpDown>
       <!-- 로그인 전 -->
       <div class="flex gap-5" v-show="!myName">
         <nuxt-link to="/auth/login" class="font-bold">로그인</nuxt-link>
