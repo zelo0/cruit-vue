74ae4847ff1286e41c2cd5b0657bf67b4c6aeb72
zelo0 <minorSunbi@gmail.com>
2022-03-27 23:37:11 +0900

    마크 다운으로 편집된 문서 보기 전용 컴포넌트 생성 (purify)

---
+++ components/MarkViewer.vue
@@ -0,0 +1,21 @@
+<template lang="">
+  <div class="ql-snow">
+    <div class="ql-editor just-view" v-dompurify-html="content"></div>
+  </div>
+</template>
+
+<script>
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+export default {
+  props: ['content'],
+  data() {
+    return {}
+  },
+}
+</script>
+<style lang="css" scoped>
+.just-view {
+  @apply px-0 py-3;
+}
+</style>
