dfbf4a07ee1edfd96ade211f381500b0b72833fe
zelo0 <minorSunbi@gmail.com>
2022-03-28 00:34:42 +0900

    ec2 url

--- nuxt.config.js
+++ nuxt.config.js
@@ -52,8 +52,8 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://localhost:8080/api/v1', // local
-    // baseURL: 'https://www.cruitapp.com/api/v1', // ec2
+    // baseURL: 'http://localhost:8080/api/v1', // local
+    baseURL: 'https://www.cruitapp.com/api/v1', // ec2
     credentials: true,
   },
 
