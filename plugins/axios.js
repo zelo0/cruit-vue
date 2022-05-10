37d9afa8433d6b23f66d5b3bb43ba769a7326a4a
zelo0 <minorSunbi@gmail.com>
2022-05-11 01:06:34 +0900

    에러 시 replace -> push

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,9 +1,9 @@
 export default function ({ $axios, app }) {
   $axios.onError((error) => {
     if (error.response.status === 401) {
-      app.router.replace('/auth/login')
+      app.router.push('/auth/login')
     } else if (error.response.status == 403) {
-      app.router.replace('/error/403')
+      app.router.push('/error/403')
     }
   })
 }
